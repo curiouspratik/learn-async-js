9ab58fe81b9ce2686e5749af33b1eb0d3fd1468a
Pratik Patil <patil.pratiq@gmail.com>
2024-02-23 18:19:30 -0500

    added changes from the lecture

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -45,6 +45,10 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify(incidents.filter((incident) => {
                 return incident.id === `MABOS00${id}`
             })));
+        } else {
+            // If the request is for the endpoint but parameters are missing, send a bad request response
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Bad request, missing parameters');
         }
     } else {
         // If the request is for an unsupported endpoint, send a not found response
