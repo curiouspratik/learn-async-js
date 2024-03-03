3f28f9738012941ae7fcbefb3d404d56de8405ba
Pratik Patil <patil.pratiq@gmail.com>
2024-03-02 21:24:36 -0500

    added changes from the lecture

--- src/quiz/animations/main.js
+++ src/quiz/animations/main.js
@@ -1,42 +1,54 @@
 const aliceTumbling = [
-    { transform: 'rotate(0) scale(1)' },
-    { transform: 'rotate(360deg) scale(0)' }
-  ];
-  
-  const aliceTiming = {
+    { transform: "rotate(0) scale(1)" },
+    { transform: "rotate(360deg) scale(0)" },
+];
+
+const aliceTiming = {
     duration: 2000,
     iterations: 1,
-    fill: 'forwards'
-  }
-  
-  const alice1 = document.querySelector("#alice1");
-  const alice2 = document.querySelector("#alice2");
-  const alice3 = document.querySelector("#alice3");
+    fill: "forwards",
+};
 
-  alice1
-    .animate(aliceTumbling, aliceTiming)
-    .finished
-    .then((res) => {
-        console.log(res);
-        alice2
-            .animate(aliceTumbling, aliceTiming)
-            .finished
-            .then((res) => {
-                console.log(res);
-                alice3.animate(aliceTumbling, aliceTiming);
-            })
-    });
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
 
-  // Promise chain  
-  // alice1.animate(aliceTumbling, aliceTiming).finished  
-  //   .then(() => {
-  //       return alice2
-  //               .animate(aliceTumbling, aliceTiming)
-  //               .finished;     
-  //   })
-  //   .then(() => {
-  //     return alice3
-  //             .animate(aliceTumbling, aliceTiming)
-  //             .finished;
-  //   })
-  //   .catch((err) => alert(`Error when promising ... ${err.message}`));
+// alice1
+//   .animate(aliceTumbling, aliceTiming)
+//   .finished
+//   .then((res) => {
+//       console.log(res);
+//       alice2
+//           .animate(aliceTumbling, aliceTiming)
+//           .finished
+//           .then((res) => {
+//               console.log(res);
+//               alice3.animate(aliceTumbling, aliceTiming);
+//           })
+//   });
+
+anim();
+
+async function anim() {
+    try {
+        await alice1.animate(aliceTumbling, aliceTiming).finished;
+        await alice2.animate(aliceTumbling, aliceTiming).finished;
+        await alice3.animate(aliceTumbling, aliceTiming).finished;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Promise chain
+// alice1.animate(aliceTumbling, aliceTiming).finished
+//   .then(() => {
+//       return alice2
+//               .animate(aliceTumbling, aliceTiming)
+//               .finished;
+//   })
+//   .then(() => {
+//     return alice3
+//             .animate(aliceTumbling, aliceTiming)
+//             .finished;
+//   })
+//   .catch((err) => alert(`Error when promising ... ${err.message}`));
--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -1,5 +1,29 @@
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, -9]
+    [7, 8, -9],
 ];
+
+const promise0 = new Promise((resolve, reject) => {
+    array2D[0].forEach((value) => {
+        if (value < 0) resolve(array2D[0]);
+    });
+    reject(false);
+});
+const promise1 = new Promise((resolve, reject) => {
+    array2D[1].forEach((value) => {
+        if (value < 0) resolve(array2D[1]);
+    });
+    reject(false);
+});
+const promise2 = new Promise((resolve, reject) => {
+    array2D[0].forEach((value) => {
+        if (value < 0) resolve(array2D[2]);
+    });
+    reject(false);
+});
+Promise.any([promise0, promise1, promise2])
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => console.log(`Error: ${error}`));
